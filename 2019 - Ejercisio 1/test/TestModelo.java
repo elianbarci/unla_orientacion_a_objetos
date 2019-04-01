f6fe513f89f1a2a08415dd09ea0041cf44c7db40
Elian Barci <eli.barci@gmail.com>
2019-04-01 19:46:12 -0300

    01/04/2019 - Hecho en clase
    Todavia hay errores en el metodo de sacar factorial y por ende, el de combinatoria

--- 2019 - Ejercisio 1/test/TestModelo.java
+++ 2019 - Ejercisio 1/test/TestModelo.java
@@ -6,11 +6,13 @@ public class TestModelo {
 
 	public static void main(String[] args) {
 		
-		Numero numero = new Numero(40);
+		Numero numero = new Numero(5);
 		System.out.println(numero.sumar(10));
 		System.out.println(numero.multiplicar(5));
 		System.out.println(numero.esPar());
 		System.out.println("PRIMO: " + numero.esPrimo());
+		System.out.println(numero.calcularFactorial());
+		numero.numeroCombinatorio(22, 10);
 
 	}
 
